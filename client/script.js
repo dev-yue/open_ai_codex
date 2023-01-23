c77c2cadeff95221c4ce3531b82f097933bb8f9c
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-01-22 22:39:28 -0600

    deployed

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://localhost:5000', {
+    const response = await fetch('https://codex-k6d3.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
